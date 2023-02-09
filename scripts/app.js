6c5f8c9d5521b98744d531701c097bdab35435a2
pedromotta462 <pedromotta1d2015@gmail.com>
2023-02-09 05:07:37 -0300

    Skills page and others adjusts

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+/* Show things */
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,3 +12,36 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* Show scrollup */
+const targetElements = document.querySelectorAll('.showscrollup');
+const button = document.getElementById('scholl-up');
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            button.classList.remove('show-scroll');
+        } else {
+            button.classList.add('show-scroll');
+        }
+    });
+});
+
+targetElements.forEach(targetElement => {
+    observer2.observe(targetElement);
+});
+
+/* Swipper */
+
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
